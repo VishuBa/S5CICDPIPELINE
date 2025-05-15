dbc57bdcec6083fab2694f76734f95c75b91b6ab
VishuBa <visveswaraiah@gmail.com>
2025-05-15 14:40:06 +0530

    please runnn

--- src/model.py
+++ src/model.py
@@ -4,14 +4,14 @@ import torch.nn as nn
 class MNISTModel(nn.Module):
     def __init__(self):
         super(MNISTModel, self).__init__()
-        # Increased number of filters and added padding
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        # Minimal architecture
+        self.conv1 = nn.Conv2d(1, 4, kernel_size=3, padding=1)  # Reduced to 4 filters
+        self.conv2 = nn.Conv2d(4, 8, kernel_size=3, padding=1)  # Reduced to 8 filters
         self.dropout1 = nn.Dropout2d(0.25)
         self.dropout2 = nn.Dropout2d(0.5)
-        self.fc1 = nn.Linear(64 * 3 * 3, 128)  # Adjusted for three conv layers
-        self.fc2 = nn.Linear(128, 10)
+        # After two maxpool layers: 28x28 -> 14x14 -> 7x7
+        self.fc1 = nn.Linear(8 * 7 * 7, 16)  # Reduced to 16 neurons
+        self.fc2 = nn.Linear(16, 10)
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(2)
         
@@ -20,9 +20,6 @@ class MNISTModel(nn.Module):
         x = self.maxpool(x)
         x = self.dropout1(x)
         x = self.relu(self.conv2(x))
-        x = self.maxpool(x)
-        x = self.dropout1(x)
-        x = self.relu(self.conv3(x))
         x = self.maxpool(x)
         x = self.dropout1(x)
         x = x.view(x.size(0), -1)
